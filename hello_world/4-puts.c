9a1c1ba4f29bdb6452426180a12362a22392afae
niniisg <nisselsanchez@gmail.com>
2024-02-19 19:22:17 -0400

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -4,9 +4,10 @@
  * main - Entry point
  *  
  *
- * Return: Always 0.
+ * Return: Always 0 (success)
  */
 int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
          return (0);
