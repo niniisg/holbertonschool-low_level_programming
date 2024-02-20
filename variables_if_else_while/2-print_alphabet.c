804b95388d44be8f165ff03f54a97b5d643ee9ce
niniisg <nisselsanchez@gmail.com>
2024-02-20 15:48:34 -0400

    2-print_alphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -8,14 +8,13 @@
 
 int main(void)
 {
-
 	char alphabet;
 
 	for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
-
-{
-		print the value of the variable
+	{
+		/*print the value of the variable*/
 		putchar(alphabet);
+	}
+	putchar('\n');
+	return(0);
 }
-putchar('\n');
-return (0);
