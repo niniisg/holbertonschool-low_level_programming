bd7f229e8af51793bc461eee8ef76229f262498e
niniisg <nisselsanchez@gmail.com>
2024-02-20 17:51:43 -0400

    9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -11,13 +11,14 @@ int main(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		putchar((num + '0');
-		if (num == 9)
+		putchar(num + '0');
+		if (num != 9)
+	{
 
 		putchar(',');
 		putchar(' ');
 	}
-
+}
 	putchar('\n');
 
 	return (0);
