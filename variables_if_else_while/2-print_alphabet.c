39f48e997c7824eb9f63b671c1d4259fe4303728
niniisg <nisselsanchez@gmail.com>
2024-02-20 15:31:56 -0400

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main -lowercase alphabet
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <='z' ; alphabet++)
+
+{
+		print the value of the variable
+		putchar(alphabet);
+}
+putchar('\n');
+return (0);
