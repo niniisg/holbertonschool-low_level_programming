28b36990831e8c5ae73a48c379f070fbad2592f0
niniisg <nisselsanchez@gmail.com>
2024-02-19 22:35:53 -0400

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,5 +1,14 @@
-#include <stdio.h>
-int main () {
-    printf("size of char: %lu bytes\n", sizeof(chard));
-    return 0;
-} 
+#include<stdio.h>
+/**
+ * main -print the size using sizeof
+ * return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf(Size of char: %lu byte(s)\n", sizeof(int));
+	printf(Size of char: %Lu byte(s)\n", sizeof(long int));
+	printf(Size of char: %lu byte(S)\n", sizeof(long long int));
+	printf(Size of char: %lu byte(s)\n", sizeof(float));
+	return (0);
+}
