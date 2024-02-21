7bf739223b42920a2b5c6e1b54ee44d79aa964c3
niniisg <nisselsanchez@gmail.com>
2024-02-21 16:02:11 -0400

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main -prints the alphabet in lowercase
+ *
+ * Return: 0 for (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z' ; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
