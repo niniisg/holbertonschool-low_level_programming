2940b3ffc6559b9d1f95ba9f72f7483048277d2a
niniisg <nisselsanchez@gmail.com>
2024-02-22 11:24:23 -0400

    0-isupper.c

---
+++ more_functions_nested_loops/main.h/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks if a character is lowercase.
+ *
+ * Return: 1 if character is lowercase, 0 otherwise.
+ * @c: The character to be checked.
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
