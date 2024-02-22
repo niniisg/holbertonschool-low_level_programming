0fed2a6dfe69cdfaaa80606118862b71d09c5055
niniisg <nisselsanchez@gmail.com>
2024-02-22 16:55:42 -0400

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * mul - multipies two integers
+ *
+ * @a: function that multiples two int
+ * @b: function that multiples two int
+ *
+ * Return: Always 0
+ *
+ */
+
+int mul(int a, int b)
+
+{
+	int esme;
+
+	esme = a * b;
+
+	return (esme);
+
+}
