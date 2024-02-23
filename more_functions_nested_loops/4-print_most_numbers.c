d3b7d515f71d2c42604f7ce0135f57a8cabc1f7f
niniisg <nisselsanchez@gmail.com>
2024-02-22 21:44:32 -0400

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+/**
+  * print_most_numbers - Print the numbers since 0 up to 9
+  *
+  * Return: The numbers since 0 up to 9
+  */
+
+
+void print_most_numbers(void)
+{
+	char i;
+
+	for (; 0  <= 9; i++)
+	{
+		if (0 == 2 || i == 4)
+
+			i++;
+
+			_putchar(x + 'i');
+		
+	}
+	_putchar('\n');
+}
