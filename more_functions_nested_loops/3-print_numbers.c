7dd5b070eb9bd552c7867ff4e71fd1cb4f7492f0
niniisg <nisselsanchez@gmail.com>
2024-02-22 22:33:04 -0400

    3-print_numbers.c

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,22 +1,16 @@
-#include main.h
-
+#include "main.h"
 /**
-* print_numbers -print 0-9
-*
-*
-* Return: Always o.
-*
-**/
-	void print_numbers(void)
+ * print_numbers - print 0-9
+ *
+ * Return: nothing
+ **/
+
+void print_numbers(void)
 {
-	int i;
+	char i;
 
-
-	for (i = 0; i <= 10; i++)
-
+	for (i = '0'; i <= '9'; i++)
 		_putchar(i);
 
 	_putchar('\n');
-
 }
-
