99e9c86d7dbe56695d1d1578bf6686577aafde6e
niniisg <nisselsanchez@gmail.com>
2024-02-22 23:12:54 -0400

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_most_numbers();
+    return (0);
+}
+julien@ubuntu:~/$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 4-main.c 4-print_most_numbers.c -o 4-print_most_numbers
+julien@ubuntu:~/$ ./4-print_most_numbers 
+01356789
