d1cebe380df71c264cb0d7909ea0d838cb7ced08
niniisg <nisselsanchez@gmail.com>
2024-02-26 17:14:05 -0400

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *
+ * return the length of string
+ *@s: string
+ */
+
+int _strlen(char *s)
+
+{
+	int i = 0;
+
+	while (s[i] != '\0');
+
+
+
+	i++;
+
+	return (i);
+
+}
+
