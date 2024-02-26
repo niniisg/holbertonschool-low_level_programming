b5574e4aeb3a34987999a0148b268d8eaa24d00c
niniisg <nisselsanchez@gmail.com>
2024-02-26 13:54:15 -0400

    fixed file 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swapping values of two integers
+ *  a: writing a function that swaps values
+ *  b: other value will be the same as a
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
+
+}
