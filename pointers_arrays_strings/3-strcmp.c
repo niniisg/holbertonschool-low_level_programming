296acac603f16e94d4b55ef5df1aab575643e635
niniisg <nisselsanchez@gmail.com>
2024-02-28 22:00:52 -0400

    3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -5,18 +5,23 @@
  * @s1: firts string to compare
  * @s2: second string to compare
  *
- * Return: if s1 is less than s2, 0 if equal
+ * Return: less than 0 is s1 is less than s2, 0 ifd they're equal
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1, char *s2)
+	int i;
+
+	i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (*s1 == '\0')
+		if (s1[i] != s2[i])
 		{
-			return (0);
+			return (s1[i] - s2[i]);
+				i++;
 		}
-		s1++;
-		s2++;
+		if (s1[i] == s2[i])
+			return (0);
+		return (s1[i] - s2[i]);
 	}
-	return (*s1 - *s2);
 }
