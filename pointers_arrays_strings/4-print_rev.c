6ad97ea08604582091630c5c22841608038cdcf5
niniisg <nisselsanchez@gmail.com>
2024-03-01 10:15:25 -0400

    fixing

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_rev - prints a string in reverse, followed by a new line
  * @s: string to be printed
--- pointers_arrays_strings/README.md
+++ pointers_arrays_strings/README.md
@@ -1,1 +1,1 @@
-pointers_arrays_strings
+pointers arrays strings
