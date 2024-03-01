3064f0d7d4083497f0ef1c8bbc9322cca9f801be
niniisg <nisselsanchez@gmail.com>
2024-02-29 23:30:38 -0400

    2-strchr.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -9,7 +9,7 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-int _putchar(char c);
+int _putchar(char c);i
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
