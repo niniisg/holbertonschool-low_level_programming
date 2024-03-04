06ba3af198a7fbfa9ce12a62e71f9e54f2e08ff8
niniisg <nisselsanchez@gmail.com>
2024-03-04 15:03:18 -0400

    0-puts_recursion.c

--- 0-puts_recursion.c
+++ 0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *_puts_recursion -prints a string followed by a new line
+ *_puts_recursion - prints a string followed by a new line
  *@s: string
  *
  *Return: always 0
@@ -10,13 +10,13 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s != '\0')
 	{
-		_putchar(*s);
+		_putchar(s[0]);
 		_puts_recursion(s + 1);
 
 	}
 	else
 	_putchar('\n');
-
+	}
 }
