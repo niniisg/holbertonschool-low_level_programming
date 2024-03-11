efa00c2ee416d3cd372f69fa519ff1d734c54381
niniisg <nisselsanchez@gmail.com>
2024-03-11 11:49:32 -0400

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog -dog attributes
+ *@name: the dogs name
+ *@age: the dogs age
+ *@Owner: the dog owner
+ *
+ */
+struct dog
+{
+char *name
+float age
+char *owner
+};
+
