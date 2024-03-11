120752753ed727dd82efad7404795c96b42f37a5
niniisg <nisselsanchez@gmail.com>
2024-03-11 19:18:21 -0400

    2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -13,6 +13,6 @@ return;
 if (d->name == NULL)
 d->name = "(nil)";
 if (d->owner == NULL)
-d->owner = "nil)";
+d->owner = "(nil)";
 printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
