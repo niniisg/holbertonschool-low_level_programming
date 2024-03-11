83aef645149979a8925d1897f256dd305ac5e34b
niniisg <nisselsanchez@gmail.com>
2024-03-11 19:41:48 -0400

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,12 +14,8 @@ typedef struct dog
 char *name;
 float age;
 char *owner;
-};
 
-/**
- * dog_t - typedef for struct dog
- */
-typedef struct dog dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
@@ -27,4 +23,4 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
-#endif /* struc dog a - structure of dog */
+#endif
