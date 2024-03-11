3b185eb92d1e478481853ed4a59cbbdba437bda2
niniisg <nisselsanchez@gmail.com>
2024-03-11 19:15:58 -0400

    2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -4,14 +4,13 @@
 /**
  * print_dog - prints a struct dog
  * @d: struct dog to print
- *
  */
 void print_dog(struct dog *d)
 {
 if (d == NULL)
 return;
 
-if (d == NULL)
+if (d->name == NULL)
 d->name = "(nil)";
 if (d->owner == NULL)
 d->owner = "nil)";
