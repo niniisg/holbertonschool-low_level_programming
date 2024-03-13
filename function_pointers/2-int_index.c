24904a511a817007d2a01b2d2d15124f6a29e111
niniisg <nisselsanchez@gmail.com>
2024-03-13 15:41:49 -0400

    2-int_index.c

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -5,7 +5,7 @@
  *@array: array
  *@size: size of elements
  *@cmp: pointer to function
- * Return: 0
+ *Return: 0
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
@@ -15,7 +15,7 @@ return (-1);
 
 for (r = 0; r < size; r++)
 {
-if (!cmp(array[r]))
+if (cmp(array[r]))
 return (r);
 }
 return (-1);
