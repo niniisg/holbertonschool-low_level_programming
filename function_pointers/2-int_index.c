906be7e9ee28396df597005e505438159deb1a80
niniisg <nisselsanchez@gmail.com>
2024-03-13 15:25:38 -0400

    2-int_index.c

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - return index
+ *@array: array
+ *@size: size of elements
+ *@cmp: pointer to function
+ * Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int r;
+if (array == NULL || size <= 0 || cmp == NULL)
+return (-1);
+
+for (r = 0; r < size; r++)
+{
+if (!cmp(array[r]))
+return (r);
+}
+return (-1);
+}
