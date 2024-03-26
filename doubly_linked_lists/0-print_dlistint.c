409b4450543a4fdf91d6bc53eab5f19f1d0d6231
niniisg <nisselsanchez@gmail.com>
2024-03-26 18:54:48 -0400

    upload files

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_dlistint -prints elements of linked list
+ * @h: pointer to the head of nodes
+ *
+ * Return: numbers of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		if (!h->n)
+		{
+			printf("nil");
+		}
+		else
+		{
+			printf("%d\n", h->n);
+		}
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
+}
---
+++ doubly_linked_lists/README.md
@@ -0,0 +1,4 @@
+Holberton school low level programming in C
+Doubly linked list is a variation of linked list in
+which navigation is possible in both ways
+forward as well as backward easily as compared to single linked list
