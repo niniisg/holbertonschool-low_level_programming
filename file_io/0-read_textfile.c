cb971176b992692bd8cbcd926626eeafbfce4eb5
niniisg <nisselsanchez@gmail.com>
2024-03-28 19:16:33 -0400

    files

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include "main.h"
+#include <unistd.h>
+/**
+ *
+ *
+ *
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	ssize_t d, o, c;
+
+	d = open(filename, O_RDONLY);
+	if (d == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+
+	if (buffer == NULL)
+		return (0);
+	o = read(d, buffer, letters);
+	c = write(STDOUT_FILENO, buffer, o);
+
+	if (c == -1 || o == -1 || o != c)
+		return (0);
+	free(buffer);
+	close(d);
+	return (c);
+}
---
+++ file_io/README.md
@@ -0,0 +1,4 @@
+Holberton school low level programming in C
+C-file |/o
+this proyect cover how to create,open,close
+text or binary files for their data storage
